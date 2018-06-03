03c21a43f3383a3311521df772af94c965a81878
Abai <abai2010@bk.ru>
2018-06-03 14:19:47 +0600

    InitComm

---
+++ test_cons/test_cons/Client.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace test_cons
+{
+    public class Client
+    {
+        public string Name { get; set; }
+        public Socket Socket { get; set; }
+    }
+}
